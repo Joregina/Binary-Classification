c066e6e3fa93a21da9f8265328387c5076f92ba3
Gina Mthembu <kimberleymthembu0@gmail.com>
2026-03-23 22:47:13 +0200

    Initial commit - add all project files

--- Split Data.R
+++ Split Data.R
@@ -1,1 +1,19 @@
+# Spliting Data -----------------------------------------------------------
+## This script illustrates the technique used to split data into 70% training data and 30% test data Machine Learning
 
+## Import libraries that will be used in this script
+library(tidyverse)
+library(caret)
+
+#Load data set
+library(readr)
+diabetes <- read_csv("diabetes.csv")
+View(diabetes)
+
+# split data into training and test
+set.seed(123)
+training.sample<- diabetes$Outcome %>%
+  createDataPartition(p=0.5, list=FALSE)
+
+train.data <- diabetes[training.sample, ]
+test.data<- diabetes[-training.sample, ]
